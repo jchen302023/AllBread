2afa52dd776a433673a58cbfeab3940835b3fe56
Jacc Chen <90912786+jchen302023@users.noreply.github.com>
2022-01-19 11:30:16 -0500

    deleted unneccesary stuffs

--- v2/Location.java
+++ v2/Location.java
@@ -20,40 +20,4 @@ public class Location {
     }
   } // inventory
 
-  // public static void leaveHome() {
-  //   String s;
-  //   s = "\n==============================\n";
-  //   s += "You have chosen to leave your humble abode. \n";
-  //   s += "Where would you like to go? \n";
-  //   s += "\t1: View Inventory \n";
-  //   s += "\t2: The Rose Garden \n";
-  //   s += "\t3: The Farmers Stand \n";
-  //   s += "\t4: The Convience Store \n";
-  //   s += "\t5: The Barn \n";
-  //   s += "\t6: The Bakery \n";
-  //   s += "\t7: Back to my humble abode \n";
-  //   System.out.println(s);
-  //
-  //  choice = scanner.nextLine();
-  //
-  //  if (choice.equals("1")) {
-  //    inventory();
-  //  } // picking choice 1
-  //   // else if (choice == 2) {
-  //   //   roseGarden();
-  //   // } // choice 2
-  //   // else if (choice == 3) {
-  //   //   roseGarden();
-  //   // } // choice 3
-  //   // else if (choice == 2) {
-  //   //   roseGarden();
-  //   // } // choice 4
-  //   // else if (choice == 2) {
-  //   //   roseGarden();
-  //   // } // choice 5
-  //   // else if (choice == 2) {
-  //   //   roseGarden();
-  //   // } // choice 6
-  //
-  // } // leaveHome
-}
+} // Location 
